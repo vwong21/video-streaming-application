8b7a04b336049bc128ef479aa824277e7be96efe
Vincent Wong <vincentwong5609@gmail.com>
2024-02-24 22:16:30 -0800

    changed auth

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -1,15 +1,15 @@
 import { Router, Route, useNavigate } from "react-router-dom";
-
 import "../App.css";
 import { useAuth } from "../contexts/AuthContext";
 import { useEffect } from "react";
 
 function App() {
   const navigate = useNavigate();
-  const authenticated = useAuth();
+  const { authenticated, login } = useAuth();
   useEffect(() => {
-    if (!authenticated) {
+    if (authenticated == false) {
       navigate("/auth");
+    } else {
       console.log(authenticated);
     }
   });
