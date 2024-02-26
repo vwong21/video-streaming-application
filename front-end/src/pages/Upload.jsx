6cebb6ef68994f7a5cf9f45904b8fec5ee960b41
Vincent Wong <vincentwong5609@gmail.com>
2024-02-26 13:20:15 -0800

    added file system service and containerized upload service

--- front-end/src/pages/Upload.jsx
+++ front-end/src/pages/Upload.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 const Upload = () => {
   const [file, setFile] = useState();
-
+  const [title, setTitle] = useState();
   const handleChange = (event) => {
     setFile(event.target.files[0]);
   };
@@ -10,9 +10,10 @@ const Upload = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3002/upload", {
-        video: file,
-      });
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("video", file);
+      const res = await axios.post("http://localhost:3002/upload", formData);
       console.log(res);
     } catch (error) {
       console.error(error);
@@ -29,6 +30,15 @@ const Upload = () => {
     <>
       <form onSubmit={submitForm}>
         <h1>React File Upload</h1>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          value={title || ""}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
         <input type="file" onChange={handleChange} />
         <button type="submit">Upload</button>
       </form>
--- upload/routes/uploadRoutes.js
+++ upload/routes/uploadRoutes.js
@@ -2,23 +2,30 @@ const express = require('express')
 const multer = require('multer')
 
 const router = express.Router()
-const videosPath = '../videos/'
+// const videosPath = '..//videos/'
+const videosPath = '/app/videos/'
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, videosPath)
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        console.log(req.body, req.body.title)
+        cb(null, `${req.body.title}.mp4`)
     },
 })
 const upload = multer({ storage })
+
+
+
 router.post('/', upload.single('video'), (req, res) => {
-    console.log(req)
-    res.status(201).json(req.file)
+    // Check if multer encountered an error during file upload
+    if (!req.file) {
+        // No file was provided or something went wrong during upload
+        return res.status(400).json({ error: 'No file uploaded or an error occurred' });
+    }
+    const title = req.body.title
+    const filePath = `${videosPath}${req.body.title}.mp4`
+    console.log(filePath)
+    res.status(201).json({ success: true, file: req.file, title: req.body.title })
 })
-
-router.get('/', (req, res) => {
-    res.status(201).json({message: "success"})
-})
-
 module.exports = router
