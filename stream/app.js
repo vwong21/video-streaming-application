f11299fde1d5aec062bff566acad6a5a0b30a71d
Vincent Wong <vincentwong5609@gmail.com>
2024-05-22 13:12:31 -0700

    configured backend to stream video

--- stream/app.js
+++ stream/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const cors = require('cors')
+const fs = require('fs')
 const app = express()
 const {getVideo} = require(process.env.DB_PATH)
 app.use(cors())
@@ -8,11 +9,39 @@ app.use(express.json())
 
 app.get('/stream', async (req, res) => {
     console.log(req.query.title)
-    const filePath = await getVideo(req.query.title)
+    const filePathObject = await getVideo(req.query.title)
+    const filePath = filePathObject.filePath
+    console.log(filePath)
     if (filePath == undefined) {
-        return res.status(401).json({message:'could not find video'})
+        return res.status(404).json({message:'could not find video'})
     }
-    return res.status(201).json({filePath})
+    const stat = fs.statSync(filePath)
+    const fileSize = stat.size
+    const range = req.headers.range
+
+    if (range) {
+        const parts = range.replace(/bytes=/, '').split('-')
+        const start = parseInt(parts[0], 10)
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+
+        const chunkSize = end - start + 1
+        const file = fs.createReadStream(filePath, {start, end})
+        const head = {
+            'Content-Range': `bytes ${start} - ${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'video/mp4'
+        }
+        res.writeHead(206, head)
+        file.pipe(res)
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4'
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(filePath).pipe(res)
+    }
 })
 
-app.listen(3003)
+app.listen(3003)    
