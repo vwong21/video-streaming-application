dc51f0b06ef07981b215b4b458679bc875bce127
Vincent Wong <vincentwong5609@gmail.com>
2024-08-09 22:30:05 -0700

    revamped frontend

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Router, Route, useNavigate, Link } from "react-router-dom";
 import "../App.css";
 import { useAuth } from "../contexts/AuthContext";
 import { useEffect } from "react";
+import VideoPlayer from "../components/VideoPlayer";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [videoTitle, setVideoTitle] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setVideoTitle(title);
+  };
+
   const navigate = useNavigate();
   const { authenticated, login } = useAuth();
   useEffect(() => {
@@ -15,22 +25,27 @@ function App() {
   });
 
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/upload"}>Upload</Link>
-          </li>
-          <li>
-            <Link to={"/stream"}>Stream</Link>
-          </li>
-        </ul>
-      </nav>
-      <h1>Home</h1>
-    </>
+    <div id="root">
+      <header>
+        <h1>StreamShelf</h1>
+      </header>
+      <main>
+        <section className="browse">
+          <form onSubmit={handleSubmit} id="search_vid">
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </section>
+        <section className="search">
+          {videoTitle && <VideoPlayer videoName={videoTitle} />}
+        </section>
+      </main>
+    </div>
   );
 }
 
